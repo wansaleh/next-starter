0bd01111c169f459581c97975333183666e52443
Wan Saleh <wansaleh@gmail.com>
2019-11-17 12:10:45 +0800

    Updates.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 const compose = require('lodash/fp/compose');
 const withCSS = require('@zeit/next-css');
+const withSASS = require('@zeit/next-sass');
 const withImages = require('next-images');
 const withFonts = require('next-fonts');
 
-module.exports = compose([withCSS, withImages, withFonts])({
+module.exports = compose([withCSS, withSASS, withImages, withFonts])({
   // target: 'serverless',
+  cssModules: true,
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import styled from '@emotion/styled';
 
 import Head from '../components/head';
 import Nav from '../components/nav';
+import styles from './index.scss';
 
 const Home = () => (
-  <Wrapper>
+  <div>
     <Head title="Home" />
     <Nav />
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next!</h1>
-      <p className="description">
+    <div className={styles.hero}>
+      <h1 className={styles.title}>Welcome to Next!</h1>
+      <p className={styles.description}>
         To get started, edit <code>pages/index.js</code> and save to reload.
       </p>
 
-      <div className="row">
-        <a className="card" href="https://github.com/zeit/next.js#how-to-use">
+      <div className={styles.row}>
+        <a
+          className={styles.card}
+          href="https://github.com/zeit/next.js#how-to-use"
+        >
           <h3>Getting Started &rarr;</h3>
           <p>Learn more about Next.</p>
         </a>
         <a
-          className="card"
+          className={styles.card}
           href="https://github.com/zeit/next.js/tree/canary/examples"
         >
           <h3>Examples &rarr;</h3>
@@ -29,55 +32,7 @@ const Home = () => (
         </a>
       </div>
     </div>
-  </Wrapper>
+  </div>
 );
 
 export default Home;
-
-const Wrapper = styled.div`
-  .hero {
-    width: 100%;
-    color: #333;
-  }
-  .title {
-    margin: 0;
-    width: 100%;
-    padding-top: 80px;
-    line-height: 1.15;
-    font-size: 48px;
-    font-weight: 200;
-  }
-  .title,
-  .description {
-    text-align: center;
-  }
-  .row {
-    max-width: 880px;
-    margin: 80px auto 40px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-  }
-  .card {
-    padding: 18px 18px 24px;
-    width: 40%;
-    text-align: left;
-    text-decoration: none;
-    color: #434343;
-    border: 1px solid #9b9b9b;
-  }
-  .card:hover {
-    border-color: #067df7;
-  }
-  .card h3 {
-    margin: 0;
-    color: #067df7;
-    font-size: 18px;
-  }
-  .card p {
-    margin: 0;
-    padding: 12px 0 0;
-    font-size: 13px;
-    color: #333;
-  }
-`;

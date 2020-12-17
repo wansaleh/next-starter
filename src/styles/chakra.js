6f961ad42e3ffc9c9aac6f176e49480eedac6e84
Wan Saleh <wansaleh@gmail.com>
2020-12-17 10:18:52 +0800

    Updates.

--- src/styles/chakra.js
+++ src/styles/chakra.js
@@ -1,49 +1,39 @@
-import theme from '@chakra-ui/theme';
+import { extendTheme } from '@chakra-ui/core';
+
 import resolveConfig from 'tailwindcss/resolveConfig';
 import tailwindConfig from '../../tailwind.config';
 
 const tailwind = resolveConfig(tailwindConfig).theme;
 
-export default {
-  ...theme,
-
+export default extendTheme({
   styles: {
-    ...theme.styles,
+    global: (props) => ({
+      body: {
+        backgroundColor: props.colorMode !== 'dark' ? 'white' : 'black',
+        fontFamily: 'body',
+        color: props.colorMode === 'dark' ? 'white' : 'gray.900'
+      },
 
-    global: (props) => {
-      return {
-        ...theme.styles.global,
-
-        body: {
-          backgroundColor: props.colorMode !== 'dark' ? 'white' : 'black',
-          fontFamily: 'body',
-          color: props.colorMode === 'dark' ? 'white' : 'gray.900'
-        },
-
-        a: {
-          color: props.colorMode === 'dark' ? 'brand.300' : 'brand.500'
-        }
-      };
-    }
+      a: {
+        color: props.colorMode === 'dark' ? 'brand.300' : 'brand.500'
+      }
+    })
   },
 
   colors: {
-    ...theme.colors,
     brand: tailwind.colors.brand,
     brandAlt: tailwind.colors.brandAlt,
     gray: tailwind.colors.gray
   },
   fonts: {
-    ...theme.fonts,
     body: tailwind.fontFamily.sans.join(','),
     heading: tailwind.fontFamily.head.join(','),
     serif: tailwind.fontFamily.serif.join(','),
     mono: tailwind.fontFamily.mono.join(',')
   },
   fontSizes: {
-    ...theme.fontSizes,
     '2xs': '11px',
     '7xl': '5rem',
     '8xl': '7rem'
   }
-};
+});

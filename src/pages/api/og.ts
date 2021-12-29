5de63a9fcef7ac76693bdff633891696ff56c605
Wan Saleh <wansaleh@gmail.com>
2021-12-29 18:47:57 +0800

    Updates.

--- src/pages/api/og.ts
+++ src/pages/api/og.ts
@@ -1,5 +1,7 @@
 import { withOGImage } from 'next-api-og-image';
 
+import { interRegular } from '@/lib/inter';
+
 export enum GeneralQueryEnum {
   'logo',
   'siteName',
@@ -70,13 +72,9 @@ const getStyle = (
 
   @font-face {
     font-family: 'Inter';
-    font-style: normal;
-    font-weight: 100 900;
-    font-display: optional;
-    src: url('/fonts/inter-var-latin.woff2') format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
-      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
-      U+2215, U+FEFF, U+FFFD;
+    font-style:  normal;
+    font-weight: normal;
+    src: url(data:font/woff2;charset=utf-8;base64,${interRegular}) format('woff2');
   }
 
   body {
